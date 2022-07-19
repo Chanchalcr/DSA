387f93e89c4e25e2b9e63b8f8b48bd22b1662feb
Chanchal Rai <91631621+Chanchalcr@users.noreply.github.com>
2022-07-19 12:13:23 +0530

    Time: 9 ms (60.05%), Space: 65.7 MB (69.26%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -1,15 +1,37 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        
         ArrayList<Integer> list = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>();
-        for(int i: nums) {
-            set.add(i);
+        int i=0;
+        while(i<nums.length) {
+            int correct = nums[i] -1;
+            if(nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
         }
-        for(int i=1; i<= nums.length; i++) {
-            if(!set.contains(i)) {
-                list.add(i);
+        for(int j=0; j< nums.length; j++) {
+            if(j+1 != nums[j]) {
+                list.add(j+1);
             }
         }
         return list;
+        // In this approach i have used an extra space!
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int i: nums) {
+        //     set.add(i);
+        // }
+        // for(int i=1; i<= nums.length; i++) {
+        //     if(!set.contains(i)) {
+        //         list.add(i);
+        //     }
+        // }
+        // return list;
+    }
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
