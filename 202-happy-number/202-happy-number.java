54385c083dced994ad751ace8ae26e465d5be7e8
Chanchal Rai <91631621+Chanchalcr@users.noreply.github.com>
2022-09-12 19:43:03 +0530

    Time: 2 ms (67.77%), Space: 42 MB (12.97%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,16 +1,30 @@
 class Solution {
+    // public boolean isHappy(int n) {
+    //     HashSet<Integer> set = new HashSet<>();
+    //     while(true) {
+    //         if(sumOfDigitSq(n) == 1) return true;
+    //         if(set.add(sumOfDigitSq(n))) {
+    //             n = sumOfDigitSq(n);
+    //         } else {
+    //             return false;
+    //         }
+    //     }
+    // }
     public boolean isHappy(int n) {
-        HashSet<Integer> set = new HashSet<>();
-        while(true) {
-            if(sumOfDigit(n) == 1) return true;
-            if(set.add(sumOfDigit(n))) {
-                n = sumOfDigit(n);
-            } else {
-                return false;
-            }
+        int slow = n;
+        int fast = n;
+        
+        do{
+            slow = sumOfDigitSq(slow);
+            fast = sumOfDigitSq(sumOfDigitSq(fast));
+        }while(slow != fast);
+        
+        if(slow == 1) {
+            return true;
         }
+        return false;
     }
-    public static int sumOfDigit(int n){
+    public static int sumOfDigitSq(int n){
         int sum=0;
         while(n!=0) {
             sum += Math.pow((n%10),2);
