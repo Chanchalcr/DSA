a933320cadaf57a4e2eee31596ff801aee368e16
Chanchal Rai <91631621+Chanchalcr@users.noreply.github.com>
2022-10-07 19:40:07 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (98.49%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -10,69 +10,39 @@
  */
 class Solution {
      public ListNode reverseKGroup(ListNode head, int k) {
-        int count = 0;
-        ListNode dummy = new ListNode();
-        dummy.next = head;
-        
-        ListNode temp = dummy;
-        
-        while(temp.next!=null){
-            temp = temp.next;
-            count++;
-        }
-        temp = dummy;
-        while(temp.next!=null){
-            if(count<k) break;
-            int nodes = k-1;
-            ListNode tempnext = temp.next;
-            ListNode first = temp.next;
-            ListNode second = first.next;
-            
-            while(nodes-- > 0){
-                ListNode next = second.next;
-                second.next = first;
-                first = second;
-                second = next;
+       
+         if(k==1 || head == null) return head;
+        ListNode one = null;
+        ListNode two = head;
+        ListNode three = head.next;
+        while(true) {
+            int flag = 0;
+            ListNode temp = two;
+            for (int i = 0; i < k; i++) {
+                if (temp == null) {
+                    flag = 1;
+                    break;
+                }
+                temp = temp.next;
             }
-            count-=k;
-            temp.next = first;
-            tempnext.next = second;
-            temp = tempnext;
+            if(flag == 1) break;
+            ListNode last = one;
+            ListNode newEnd = two;
+            for (int i = 0; two != null && i < k; i++) {
+                two.next = one;
+                one = two;
+                two = three;
+                if (three != null) three = three.next;
+            }
+            if (last != null) {
+                last.next = one;
+            } else {
+                head = one;
+            }
+            newEnd.next = two;
+            one = newEnd;
             
-        }
-        return dummy.next;
-//          if(k==1 || head == null) return head;
-//         ListNode one = null;
-//         ListNode two = head;
-//         ListNode three = head.next;
-//         while(true) {
-//             int flag = 0;
-//             ListNode temp = two;
-//             for (int i = 0; i < k; i++) {
-//                 if (temp == null) {
-//                     flag = 1;
-//                     break;
-//                 }
-//                 temp = temp.next;
-//             }
-//             if(flag == 1) break;
-//             ListNode last = one;
-//             ListNode newEnd = two;
-//             for (int i = 0; two != null && i < k; i++) {
-//                 two.next = one;
-//                 one = two;
-//                 two = three;
-//                 if (three != null) three = three.next;
-//             }
-//             if (last != null) {
-//                 last.next = one;
-//             } else {
-//                 head = one;
-//             }
-//             newEnd.next = two;
-//             one = newEnd;
-            
-//     }
-//          return head;
+    }
+         return head;
 }
 }
