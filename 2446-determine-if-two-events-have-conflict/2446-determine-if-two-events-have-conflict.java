ec0d8cad7111ea4791b26f1efe5a4fde44bc9c44
Chanchal Rai <91631621+Chanchalcr@users.noreply.github.com>
2022-10-25 16:56:01 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (92.57%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
@@ -1,22 +1,24 @@
 class Solution {
     public boolean haveConflict(String[] event1, String[] event2) {
-        int[] range1 = new int[2]; 
-        int[] range2 = new int[2]; 
-        range1[0] = toMinute(event1[0]);
-        range1[1] = toMinute(event1[1]);
-        range2[0] = toMinute(event2[0]);
-        range2[1] = toMinute(event2[1]);
-        // if(range1[0] <= range2[0] && range2[0] <= range1[1]) return true;
-        // if(range1[0] <= range2[1] && range2[1] <= range1[1]) return true;
-        // if(range2[0] <= range1[0] && range1[0] <= range2[1]) return true;
-        // if(range2[0] <= range1[1] && range1[1] <= range2[1]) return true;
-        // return false;
-        return range2[1] >= range1[0] && range1[1] >= range2[0];
-    }
-    public int toMinute(String S) {
-        char[] event = S.toCharArray();
-        int min = 0;
-        min = ((event[0]-'0')*10 + (event[1] - '0'))*60;
-        return min + (event[3] - '0')*10 + (event[4]-'0');
+        
+        return event1[0].compareTo(event2[1]) <= 0 && event2[0].compareTo(event1[1]) <=0;
+    //     int[] range1 = new int[2]; 
+    //     int[] range2 = new int[2]; 
+    //     range1[0] = toMinute(event1[0]);
+    //     range1[1] = toMinute(event1[1]);
+    //     range2[0] = toMinute(event2[0]);
+    //     range2[1] = toMinute(event2[1]);
+    //     return range2[1] >= range1[0] && range1[1] >= range2[0];
+    //     // if(range1[0] <= range2[0] && range2[0] <= range1[1]) return true;
+    //     // if(range1[0] <= range2[1] && range2[1] <= range1[1]) return true;
+    //     // if(range2[0] <= range1[0] && range1[0] <= range2[1]) return true;
+    //     // if(range2[0] <= range1[1] && range1[1] <= range2[1]) return true;
+    //     // return false;
+    // }
+    // public int toMinute(String S) {
+    //     char[] event = S.toCharArray();
+    //     int min = 0;
+    //     min = ((event[0]-'0')*10 + (event[1] - '0'))*60;
+    //     return min + (event[3] - '0')*10 + (event[4]-'0');
     }
 }
